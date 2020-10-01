36137610d778419a00a9d3cf46dad7e36041d855
XeniaOlshanskaya <ks14012000@gmail.com>
2020-10-01 12:15:57 +0700

    Made reading and writing

--- src/AccountManagerImpl.java
+++ src/AccountManagerImpl.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AccountManagerImpl implements MailAccountManager {
 
     public AccountManagerImpl(){
@@ -7,7 +14,39 @@ public class AccountManagerImpl implements MailAccountManager {
     @Override
     public void registerNewAccount(String email, String password, Person person)
             throws DuplicateAccountException {
-        
+        File file = new File("C:/Users/123/Desktop/email2.csv");
+        ArrayList<String[]> listOfMassives= new ArrayList<>();
+        String strToWrite = "";
+
+        try {
+            FileReader fr = new FileReader(file);
+            Scanner scan = new Scanner(fr);
+            while(scan.hasNextLine()) {
+                String[] massive = scan.nextLine().split(";", 0);
+                listOfMassives.add(massive);
+                if(massive[0].equals(email)) {
+                    throw new DuplicateAccountException();
+                }
+            }
+
+            FileWriter fw = new FileWriter(file);
+
+            for(String[] element : listOfMassives) {
+                strToWrite += element[0] + ";" + element[1] + ";" + element[2]
+                        + ";" + element[3] + "\n";
+            }
+            strToWrite += email + ";" + password + ";" + person.getInitials() +
+                    ";" + person.getBirthdayDate() + "\n";
+
+            fw.append(strToWrite);
+
+            fr.close();
+            fw.close();
+
+        }
+        catch(IOException exc) {
+            exc.printStackTrace();
+        }
     }
 
     @Override
